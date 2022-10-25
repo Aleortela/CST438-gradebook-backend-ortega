7273edf486641b84ab05f179dd831fb80cd5781b
Al <36622462+Aleortela@users.noreply.github.com>
2022-10-25 00:33:14 -0700

    Minor changes

--- cst438_gradebook/src/main/java/com/cst438/controllers/InstructorController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/InstructorController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,9 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.cst438.domain.Assignment;
 import com.cst438.domain.AssignmentGrade;
+import com.cst438.domain.AssignmentListDTO;
 import com.cst438.domain.AssignmentDTO;
+
 import com.cst438.domain.AssignmentRepository;
 import com.cst438.domain.Course;
 import com.cst438.domain.CourseRepository;
@@ -37,21 +40,29 @@ public class InstructorController{
 	CourseRepository courseRepository;
 	
 	@Transactional
-	@PostMapping("/instructor/add")
-	public void createAssignment(@RequestBody AssignmentDTO adto){
+	@PostMapping("/add")
+	public void createAssignment(@RequestBody AssignmentListDTO.AssignmentDTO adto){
 		
 		Optional<Assignment> a = assignmentRepository.findById(adto.assignmentId);
 		
 		if(a.isEmpty()) {
-			Assignment ta = new Assignment(adto);
-			assignmentRepository.save(ta);
+			Course c = courseRepository.findById(adto.courseId).orElse(null);
+			if(c == null) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No courses matching entered ID");
+			}else {
+				Assignment ta = new Assignment(adto);
+				ta.setCourse(c);
+				ta.setNeedsGrading(1);
+				assignmentRepository.save(ta);
+			}
+			
 		}else {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Assignment already exists");
 		}
 	}
 	
 	@Transactional
-	@PostMapping("/instructor/delete/{id}")
+	@DeleteMapping("/delete/{id}")
 	public void updateAssignment(@PathVariable("id") Integer assignmentId) {
 		Optional<Assignment> check = assignmentRepository.findById(assignmentId);
 		
@@ -68,8 +79,8 @@ public class InstructorController{
 	}
 	
 	@Transactional
-	@PutMapping("/instructor/update")
-	public void updateAssignment (@RequestBody AssignmentDTO assignment) {
+	@PutMapping("/update")
+	public void updateAssignment (@RequestBody AssignmentListDTO.AssignmentDTO assignment) {
 		
 		Optional<Assignment> op = assignmentRepository.findById(assignment.assignmentId);
 		
