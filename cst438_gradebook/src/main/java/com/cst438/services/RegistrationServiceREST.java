60987fa0c57ce15f6e6b674a5ed32f73e5b96563
Al <36622462+Aleortela@users.noreply.github.com>
2022-10-25 00:34:32 -0700

    Implementation of sendFinalGrades method

--- cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceREST.java
+++ cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceREST.java
@@ -1,7 +1,20 @@
 package com.cst438.services;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
+
+
 
 import com.cst438.domain.CourseDTOG;
 
@@ -21,6 +34,12 @@ public class RegistrationServiceREST extends RegistrationService {
 	public void sendFinalGrades(int course_id , CourseDTOG courseDTO) { 
 		
 		//TODO  complete this method in homework 4
+		System.out.println("Service Rest: Sending final grades for " + course_id+" "+courseDTO);
+		restTemplate.put(registration_url+"/course/"+course_id, courseDTO);	
+		System.out.println("Sent final grades "+courseDTO.grades);
+		
+		
 		
 	}
 }
+
